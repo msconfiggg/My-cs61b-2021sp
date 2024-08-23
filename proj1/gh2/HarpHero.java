405074a3cc9172b47fb32f22d4b9dd885b1c26f2
msconfiggg <1466876532@qq.com>
2024-08-23 16:15:37 +0800

    proj1 commit

---
+++ proj1/gh2/HarpHero.java
@@ -0,0 +1,40 @@
+package gh2;
+
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+import deque.ArrayDeque;
+
+public class HarpHero {
+    public static final double CONCERT_A = 440.0;
+    public static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+
+    public static void main(String[] args) {
+        ArrayDeque<Harp> strings = new ArrayDeque<>();
+        for (int i = 0; i < keyboard.length(); i++) {
+            double frequency = CONCERT_A * Math.pow(2, (i - 24) / 12.0);
+            Harp string = new Harp(frequency);
+            strings.addLast(string);
+        }
+
+        while (true) {
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int index = keyboard.indexOf(key);
+                if (index >= 0) {
+                    strings.get(index).pluck();
+                }
+            }
+
+            double sample = 0.0;
+            for (int i = 0; i < keyboard.length(); i++) {
+                sample += strings.get(i).sample();
+            }
+
+            StdAudio.play(sample);
+
+            for (int i = 0; i < keyboard.length(); i++) {
+                strings.get(i).tic();
+            }
+        }
+    }
+}
