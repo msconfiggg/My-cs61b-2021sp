21cd05bbf831bb8e47543cc71343cd70d104458c
msconfiggg <1466876532@qq.com>
2024-08-25 15:05:30 +0800

    lab4B commit

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,12 +5,12 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
+        } else if (n % 2 == 1) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return n / 2;
         }
     }
 
@@ -24,3 +24,4 @@ public class Collatz {
         System.out.println();
     }
 }
+
