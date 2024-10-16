e8e4de8d709c8e6a4e69a2b65b7e31aa402bd3ee
msconfiggg <1466876532@qq.com>
2024-10-16 21:10:24 +0800

    lab12 commmit

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -1,6 +1,4 @@
 package byow.lab12;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
@@ -12,4 +10,112 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 80;
+    private static final int HEIGHT = 60;
+
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+
+    public static void addTessHexagen(TETile tile, int outSize, int inSize, int x, int y, TETile[][] world) {
+        drawLeftHalf(tile, outSize, inSize, x, y, world);
+        drawRightHalf(tile, outSize, inSize, x, y, world);
+    }
+
+    public static void drawLeftHalf(TETile tile, int outSize, int inSize, int x, int y, TETile[][] world) {
+        for (int i = 0; i < outSize; i++) {
+            int num = outSize + i;
+            int startX = x + (inSize * 2 - 1) * i;
+            int startY = y + (outSize - 1 - i) * inSize;
+            drawOneCol(tile, num, inSize, startX, startY, world);
+        }
+    }
+
+    public static void drawRightHalf(TETile tile, int outSize, int inSize, int x, int y, TETile[][] world) {
+        for (int i = 1; i < outSize; i++) {
+            int num = 2 * outSize - 1 - i;
+            int startX = x + (inSize * 2 - 1) * (outSize - 1 + i);
+            int startY = y + i * inSize;
+            drawOneCol(tile, num, inSize, startX, startY, world);
+        }
+    }
+
+    public static void drawOneCol(TETile tile, int num, int inSize, int x, int y, TETile[][] world) {
+        int startX = x;
+        for (int i = 0; i < num; i++) {
+            int startY = y + inSize * 2 * i;
+            addHexagon(tile, inSize, startX, startY, world);
+        }
+    }
+
+    public static void addRandomHexagon(int size, int x, int y, TETile[][] world) {
+        TETile tile = randomTile();
+        addHexagon(tile, size, x, y, world);
+    }
+
+    public static void addHexagon(TETile tile, int size, int x, int y, TETile[][] world) {
+        drawUpHalf(tile, size, x, y, world);
+        drawDownHalf(tile, size, x, y, world);
+    }
+
+    public static void drawUpHalf(TETile tile, int size, int x, int y, TETile[][] world) {
+        int startX = x;
+        int maxNum = 3 * size - 2;
+        for (int i = 0; i < size; i++) {
+            int num = maxNum - 2 * i;
+            int startY = y + size + i;
+            drawOneRow(tile, num, maxNum, startX, startY, world);
+        }
+    }
+
+    public static void drawDownHalf(TETile tile, int size, int x, int y, TETile[][] world) {
+        int startX = x;
+        int maxNum = 3 * size - 2;
+        for (int i = 0; i < size; i++) {
+            int num = size + 2 * i;
+            int startY = y + i;
+            drawOneRow(tile, num, maxNum, startX, startY, world);
+        }
+    }
+
+    public static void drawOneRow(TETile tile, int num, int maxNum, int x, int y, TETile[][] world) {
+        int j = y;
+        int start = x + (maxNum - num) / 2;
+        int end = x + (maxNum + num) / 2;
+        for (int i = start; i < end; i++) {
+            world[i][j] = tile;
+        }
+    }
+
+    public static void initWorld(TETile[][] world) {
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                world[i][j] = Tileset.NOTHING;
+            }
+        }
+    }
+
+    private static TETile randomTile() {
+        int tileNum = RANDOM.nextInt(6);
+        return switch (tileNum) {
+            case 0 -> Tileset.GRASS;
+            case 1 -> Tileset.FLOWER;
+            case 2 -> Tileset.SAND;
+            case 3 -> Tileset.WATER;
+            case 4 -> Tileset.MOUNTAIN;
+            case 5 -> Tileset.TREE;
+            default -> Tileset.NOTHING;
+        };
+    }
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] hexWorld = new TETile[WIDTH][HEIGHT];
+        initWorld(hexWorld);
+
+        addTessHexagen(randomTile(), 3, 3, 0, 0, hexWorld);
+
+        ter.renderFrame(hexWorld);
+    }
 }
--- proj3/byow/lab12/project3prep.md
+++ proj3/byow/lab12/project3prep.md
@@ -4,22 +4,41 @@
 After looking at your own implementation, consider the implementation provided near the end of the lab.
 How did your implementation differ from the given one? What lessons can be learned from it?**
 
-Answer:
+Answer:In my implementation, I used the bottom-left point as the anchor, whereas you used the top-left point as the anchor.
 
 -----
 
 **Can you think of an analogy between the process of tessellating hexagons and randomly generating a world using rooms and hallways?
 What is the hexagon and what is the tesselation on the Project 3 side?**
 
-Answer:
+Answer:The hexagon corresponds to the room in Project 3, while the tessellation of hexagons corresponds to the layout of rooms and hallways.
 
 -----
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually get to tessellating hexagons.**
 
-Answer:
+Answer:A method to generate a room.
 
 -----
 **What distinguishes a hallway from a room? How are they similar?**
 
-Answer:
+Answer:Differences:
+
+Function: Rooms are typically enclosed spaces used to house players or objects, while hallways are passages that connect rooms, primarily providing movement paths.
+
+Shape: Rooms are usually larger rectangles or other polygonal shapes, whereas hallways are typically narrow linear paths with a more confined shape.
+
+Layout: Rooms can exist independently, but hallways must connect two or more rooms, serving as a bridge between them.
+
+Similarities:
+
+Structure: Both rooms and hallways are fundamental units in world generation. They are part of the world and contribute to the overall map structure.
+
+Generation method: In the world generation process, both rooms and hallways can be generated through similar procedural methods, including randomly determining their size and position.
+
+
+
+
+
+
+
